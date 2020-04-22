ce2b2329753a6535939716b5e9ca7e4ddca0dbb7
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-04-22 21:38:44 +1000

    Fetch Rates & Update DOM

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+
 function calculate() {
   fetch('items.json')
     .then((res) => res.json())
