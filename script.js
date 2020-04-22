49f370268a19fd1ddbaa596c09d520ac2f972dc1
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-04-22 21:45:15 +1000

    Fetch Rates & Update DOM

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-const currencyEl_one = document.getElementById('currency-one');
 const amountEl_one = document.getElementById('amount-one');
-const currencyEl_two = document.getElementById('currency-two');
 const amountEl_two = document.getElementById('amount-two');
+const currencyEl_one = document.getElementById('currency-two');
+const currencyEl_two = document.getElementById('currency-two');
 
-function calculate() {
-  fetch('items.json')
-    .then((res) => res.json())
-    .then((data) => console.table(data));
-}
+const rateEl = document.getElementById('rate');
+const swapEl = document.getElementById('swap');
+
+// Fetch Exchange Rate & Update The Dom
+function calculate() {}
+
+// Event Listeners
+amountEl_one.addEventListener('input', calculate);
+amountEl_two.addEventListener('input', calculate);
+currencyEl_one.addEventListener('change', calculate);
+currencyEl_two.addEventListener('change', calculate);
 
 calculate();
